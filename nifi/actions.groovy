f38166accf263b60ee372ee28c50fe9c0dac2c3b
Peter Crossley <pete.crossley@oracle.com>
2018-10-23 21:50:24 -0700

    new v1 math

--- nifi/actions.groovy
+++ nifi/actions.groovy
@@ -21,81 +21,69 @@ def slurper = new groovy.json.JsonSlurper()
 flowFile = session.write(flowFile,
         { inputStream, outputStream ->
             data = slurper.parse(inputStream)
-            // if (data.rfid_tag !=null && data.movement_z >= 5 && data.movement_x >= 4 && data.movement == "activity") {
-            //     data.action = "pass"
-            //     //map.put(data.rfid_tag, (String.valueOf(System.currentTimeMillis()) + ":" + data.action))
-            // } else
-            if (data.rfid_tag !=null && data.m_z >= 5 && data.m_x >= 4 && data.movement == "knock") {
-                data.action = "pass"
-                props.setProperty(data.rfid_tag, "catch")
-            }
-
-            //get the state for a preivous throw, do they have catch.
-            if (data.rfid_tag_source != null && props.getProperty(data.rfid_tag_source) != null) {
-              //get the last action and attribute it.
-              //def last_action = state.get(data.rfid_tag_source)
-              //def action_array = last_action.split(":")
-              // if (System.currentTimeMillis() - Long.parseLong(action_array[0]) <= 30000) {
-              //   data.action = action_array[1]
-              // }
-              data.action = props.getProperty(data.rfid_tag_source)
-              props.remove(data.rfid_tag_source)
-            }
-
+            
             //calulate speed
-            if (data.rfid_tag !=null && ((data.movement == "activity" || data.movement == "knock") || data.action == "catch")) {
-              def vObjJson = '{"ax0" : 0, "t": 0, "j": 0, "vavg": 0, "vcount": 0}'
-              if (props.getProperty(data.rfid_tag + ":v") != null) {
-                vObjJson = props.getProperty(data.rfid_tag + ":v")
-              }
-              if (props.getProperty(data.rfid_tag_source + ":source:v") != null) {
-                vObjJson = props.getProperty(data.rfid_tag_source + ":source:v")
-                //clear it
-                props.remove(data.rfid_tag_source + ":source:v")
+            if (((data.movement == "activity" || data.movement == "knock") || data.action == "catch")) {
+              def vObjJson = '{"ax0" : 0, "ay0" : 0, "az0" : 0, "t": 0, "j": 0, "vavg": 0, "vcount": 0, "rfid_tag": ""}'
+              if (props.getProperty(data.dcsvid + ":v") != null) {
+                vObjJson = props.getProperty(data.dcsvid + ":v")
               }
 
               def vObj = slurper.parseText(vObjJson)
-              if ((data.m_time >= vObj.t) && (vObj.vcount == 0 || (data.m_time - vObj.t) <= 5000)) {
-                if (vObj.vcount == 0 || vObj.j == null) {
-                  vObj.j = data.m_time;
-                }
-                //seed with the current speed if its the first time
-                def v = data.m_x
-                def pt = 20; //give them 20ms by default
-                if (vObj.vcount != 0) {
-                  //v = ax0 + ax1t
-                  v = vObj.ax0 + (data.m_x * (data.m_time - vObj.t))  
-                  pt = data.m_time - vObj.j
-                }
-                vObj.t = data.m_time
-                vObj.ax0 = data.m_x
-                vObj.vavg = vObj.vavg + v
-                vObj.vcount++;
-
-                //update map
-                if (data.rfid_tag != null && data.movement == "knock") {
-                  //reset to start a new calc
-                  props.remove(data.rfid_tag + ":v")
-                  //ensure it on the source for the catcher event
-                  props.setProperty(data.rfid_tag + ":source:v", groovy.json.JsonOutput.toJson(vObj))
-                } else {
-                  props.setProperty(data.rfid_tag + ":v", groovy.json.JsonOutput.toJson(vObj))
-                }
-
-                //add data elements
-                data.velocity_x = java.lang.Math.abs((vObj.vavg / vObj.vcount));
-                data.force = data.m_x * 350;
-                data.p_time = pt
-              } else if (data.action == "catch") {
-                //add data elements
-                data.velocity_x = java.lang.Math.abs((vObj.vavg / vObj.vcount));
-                data.force = vObj.ax0 * 350;
-                data.p_time = 20 //give 1/2 sec by default
-              } else {
+              // too long between window reset or new person
+              if ((data.m_time - vObj.t) >= 5000 || data.rfid_tag != vObj.rfid_tag) {
+                vObjJson = '{"ax0" : 0, "ay0" : 0, "az0" : 0, "t": 0, "j": 0, "vavg": 0, "vcount": 0, "rfid_tag": ""}'
                 data.p_time = 20 //give 1/2 sec by default
                 //reset
-                props.remove(data.rfid_tag + ":v")
+                props.remove(data.dcsvid + ":v")
+                vObj = slurper.parseText(vObjJson)
               }
+
+              if (vObj.vcount == 0 || vObj.j == null) {
+                vObj.j = data.m_time;
+              }
+              //seed with the current speed if its the first time
+              def vx = data.m_x
+              def vy = data.m_y
+              def vz = data.m_z
+              def pt = 20; //give them 20ms by default
+              def v = 0
+              def btop = 0
+              def bbot = 0
+
+              if (vObj.vcount != 0) {
+                //v = ax0 + ax1t
+                vx = vObj.ax0 + (data.m_x * (data.m_time - vObj.t))  
+                vy = vObj.ay0 + (data.m_y * (data.m_time - vObj.t))  
+                vz = vObj.az0 + (data.m_z * (data.m_time - vObj.t))
+                //v = java.lang.Math.sqrt(java.lang.Math.pow(vx,2) + java.lang.Math.pow(vy,2) + java.lang.Math.pow(vz,2))
+                btop = btop + ((data.m_time - vObj.t) * v)
+                bbot = bbot + java.lang.Math.pow((data.m_time - vObj.t),2)
+                pt = data.m_time - vObj.j
+              }
+
+              v = java.lang.Math.sqrt(java.lang.Math.pow(vx,2) + java.lang.Math.pow(vy,2) + java.lang.Math.pow(vz,2))
+              
+              vObj.t = data.m_time
+              vObj.ax0 = data.m_x
+              vObj.ay0 = data.m_y
+              vObj.az0 = data.m_z
+              vObj.vavg = vObj.vavg + vx
+              vObj.vcount++;
+
+              //update map
+              if (data.movement == "knock") {
+                //reset to start a new calc
+                props.remove(data.dcsvid + ":v")
+              } else {
+                props.setProperty(data.dcsvid + ":v", groovy.json.JsonOutput.toJson(vObj))
+              }
+
+              //add data elements
+              data.velocity_x = v //java.lang.Math.abs((vObj.vavg / vObj.vcount));
+              data.velocity_avg = java.lang.Math.abs((vObj.vavg / vObj.vcount));
+              data.force = data.m_x * 350;
+              data.p_time = pt 
             }
             //sm.setState(map, org.apache.nifi.components.state.Scope.LOCAL)
             outputStream.write(groovy.json.JsonOutput.toJson(data).getBytes("UTF-8"))
